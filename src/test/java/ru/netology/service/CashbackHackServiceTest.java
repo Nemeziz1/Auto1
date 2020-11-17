e203a06016fd2e2c605bf8fa619e7593cb996540
Andrey Koryakin <football2331lm@gmail.com>
2020-11-17 21:00:29 +0300

    testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    void calculateRemainIfAmountLess1000() {
+       CashbackHackService service = new CashbackHackService();
+       int amount = 900;
+       int actual = service.remain(amount);
+       int expected = 100;
+       assertEquals(actual, expected);
+    }
+
+    @Test
+    void calculateIfAmountMore1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+        int actual = service.remain(amount);
+        int expected = 900;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void calculateIfAmountEquals1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+}
